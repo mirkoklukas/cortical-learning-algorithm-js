01dcd8654ac69737d881155ceda0191615e878d7
mirkoklukas <mirko.klukas@gmail.com>
2015-02-22 19:55:16 +0100

    changed example a bit

--- example-pooler.js
+++ example-pooler.js
@@ -8,16 +8,22 @@ var layer = new NeuroLayer().randomInit(10,4,2,4);
 // console.log(layer)
 
 var pooler = new cla.TemporalPooler()
-					.setNeuroLayer(layer)
-					.setActivationThreshold(3);
-					
-
-          
+          .setNeuroLayer(layer)
+          .setActivationThreshold(3);
+  
 var inputs = [
   [0,1,2],
   [3,4,5],
   [0,1,2],
   [3,4,5],
+  [],
+  [],
+  [0,1,2],
+  [3,4,5],
+  [0,1,2],
+  [3,4,5],
+  [],
+  [],
   [0,1,2],
   [3,4,5],
   [0,1,2],
@@ -38,30 +44,39 @@ var inputs = [
   [3,4,5],
   [0,1,2],
   [3,4,5],
-  [0,1,2]
+  [0,1,2],
+  [3,4,5],
+  [0,1,2],
+  [3,4,5],
+  [0,1,2],
+  [],
+  []
 ];
 
-inputs.map(function (input) {
-  // console.log('---------------------------')
+console.log('Without learning')
+
+inputs.slice(0,4).map(function (input) {
   pooler.processInput(input);
-  // pooler.inspectPrediction(-1);
   pooler.inspectInput();
   pooler.inspectActivity();
   pooler.inspectPrediction();
+})
+
+console.log('Learn... not showing that though')
+inputs.map(function (input) {
+  pooler.processInput(input);
   pooler.learn()
 })
 
-console.log('---------------------------')
+console.log('Lets take a look again')
 inputs.slice(0,4).map(function (input) {
   pooler.processInput(input);
-  // pooler.inspectPrediction(-1);
   pooler.inspectInput();
   pooler.inspectActivity();
   pooler.inspectPrediction();
-  // pooler.learn()
 })
 
-module.exports = pooler;
+
 
 
 
